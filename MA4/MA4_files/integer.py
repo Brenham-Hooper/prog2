03bc746a12eb8e16636c296d1742bbe16a4cea89
Brenham-Hooper <70558306+Brenham-Hooper@users.noreply.github.com>
2021-10-12 09:08:53 -0400

    Update integer.py

--- MA4/MA4_files/integer.py
+++ MA4/MA4_files/integer.py
@@ -11,6 +11,7 @@ class Integer(object):
 		lib.Integer_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Integer_delete.argtypes = [ctypes.c_void_p]
 		lib.Integer_fib.argtypes = [ctypes.c_int]
+		lib.Integer_fib.restype = ctypes.c_int
 		lib.Integer_fibb.argtypes = [ctypes.c_int]
 		self.obj = lib.Integer_new(val)
 
