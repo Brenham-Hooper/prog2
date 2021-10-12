d93fb6dcd53bd1b586a830e4e8da1a645ce82e29
Brenham-Hooper <70558306+Brenham-Hooper@users.noreply.github.com>
2021-10-12 08:06:52 -0400

    Update integer.cpp

--- MA4/MA4_files/integer.cpp
+++ MA4/MA4_files/integer.cpp
@@ -6,6 +6,7 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
+		int fib(int);
 	private:
 		int val;
 	};
@@ -17,7 +18,17 @@ Integer::Integer(int n){
 int Integer::get(){
 	return val;
 	}
- 
+
+int Integer::fib(int n){
+	if((n==0||n==1)){
+		return(n);     // these are the fib base cases, returns x with x=0 or x=1
+	}
+	else{
+		return(fib(n-1)+fib(n-2));
+	}
+}
+
+
 void Integer::set(int n){
 	val = n;
 	}
@@ -26,6 +37,7 @@ void Integer::set(int n){
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
+	int Integer_fib(Integer* integer, int n) {return integer->fib(n);}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
