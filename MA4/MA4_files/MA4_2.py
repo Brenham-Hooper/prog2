97e9db7617101c84b467b1d46599eb70718ddc25
Brenham-Hooper <70558306+Brenham-Hooper@users.noreply.github.com>
2021-10-13 07:46:16 -0400

    Update MA4_2.py

--- MA4/MA4_files/MA4_2.py
+++ MA4/MA4_files/MA4_2.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
+def fib_py(n):
+	if n<=1:
+		return n
+	else:
+		return(fib_py(n-1)+fib_py(n-2)) #the provided code
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-	print(f.fib())
+	x,y = [range(30,46,1)], []
+	for elt in range(16):		#finds the time taken for each c fib run by indexing upwards
+		f = Integer(30+elt)
+		start = pc()
+		f.fib()
+		end = pc()
+		y.append(end-start)
+	x1, y1 = [range(30,46,1)], []
+	for elt in range(30,46,1):		#does the same for the python function
+		start = pc()
+		fib_py(elt)
+		end = pc()
+		y1.append(end-start)
+	plt.scatter(x,y, c='b', marker='x', label='1')
+	plt.scatter(x, y, c='r', marker='s', label='-1')
+	plt.legend(loc='upper left')
+	plt.savefig("test.png")
 
 if __name__ == '__main__':
 	main()
